7d5402b8973bfc75e88719f847c02ea786175aa3
cosisaxis <kishanelewis2@gmail.com>
2022-04-10 21:52:15 -0500

    navbar working

---
+++ src/components/Nav.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useState } from 'react'
+import { icons } from 'react-icons'
+import {FaBars, FaTimes} from 'react-icons/fa' 
+import './Nav.css'
+
+const Nav = () => {
+const [buttonClick, setButtonClick] = useState(false);
+
+const clicker = () => setButtonClick(!buttonClick)
+    
+
+
+  return (
+    <div className='head'>
+        <div className='container'>
+            <h1>COINEX</h1>
+            <ul className={buttonClick? 'menu active': 'menu'}>
+                <li>
+                    <a href='/'>Home</a>
+                </li>
+                <li>
+                    <a href='/'>Feature</a>
+                </li>
+                <li>
+                    <a href='/'>Earn</a>
+                </li>
+                <li>
+                    <a href='/'>Contact</a>
+                </li>
+            </ul>
+            <div className='btn-nav'>
+                <button className ='btn'>Connect Wallet</button>
+            </div>
+            <div className='burger' onClick={clicker}>
+                <FaBars size={20} style ={{color: '#333' }}/>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
