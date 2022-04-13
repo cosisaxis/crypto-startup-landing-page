304ef0f2a65f4a30cc693c52a642ae641b7ec6ca
cosisaxis <kishanelewis2@gmail.com>
2022-04-13 16:54:08 -0500

    wallet-connected

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,13 +3,33 @@ import { useState } from 'react'
 import { icons } from 'react-icons'
 import {FaBars, FaTimes} from 'react-icons/fa' 
 import './Nav.css'
+import { ethers } from 'ethers'
+
 
 const Nav = () => {
+const[errorMsg, setErrorMsg] = useState(null);
+const[defaultAcc, setDefaultAcc] = useState(null);
+const [connectText, setConnectText] = useState('Connect')
 const [buttonClick, setButtonClick] = useState(false);
-
 const clicker = () => setButtonClick(!buttonClick)
-    
 
+const walletConnect = () =>{
+    if(window.ethereum){
+        window.ethereum.request({method: "eth_requestAccounts"})
+        .then(result =>{
+            accountChangeHandler(result[0]);
+            setConnectText('Connected') 
+        })
+    } else {
+        setErrorMsg('need to install metamask');
+    }
+}
+
+const accountChangeHandler =(newAccount) =>{
+    setDefaultAcc(newAccount)
+
+}
+ 
 
   return (
     <div className='head'>
@@ -29,17 +49,19 @@ const clicker = () => setButtonClick(!buttonClick)
                     <a href='/'>Contact</a>
                 </li>
             </ul>
+            
             <div className='btn-nav'>
-                <button className ='btn'>Connect Wallet</button>
+                <button className ='btn' onClick={walletConnect}>{connectText}</button>
+                {/* <h3>{defaultAcc}</h3> */}
             </div>
             <div className='burger' onClick={clicker}>
             {buttonClick ? (<FaTimes size={20} style={{color: '#333'}}/>) : (<FaBars size={20} style={{color: '#333'}} />)}
-                
-
             </div>
         </div>
     </div>
   )
+
+  
 }
 
 export default Nav
